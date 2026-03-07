47fc21f6ad7c00dc2ab8394d1bfe1f40cfd28cfb
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-07 10:09:23 -0300

    feat: useCase para criação do usuário;

--- ProductClientHub.Application/UseCases/Clients/Register/RegisterClientUseCase.cs
+++ ProductClientHub.Application/UseCases/Clients/Register/RegisterClientUseCase.cs
@@ -1,13 +1,52 @@
-﻿using ProductClientHub.Communication.Requests;
+﻿using Mapster;
+using ProductClientHub.Communication.Requests;
 using ProductClientHub.Communication.Responses;
+using ProductClientHub.Domain.Entities;
 using ProductClientHub.Domain.Extensions;
+using ProductClientHub.Domain.Repositories.Client.Register;
+using ProductClientHub.Domain.Repositories.UnitOfWork;
 using ProductClientHub.Exceptions.ExceptionsBase;
 
 namespace ProductClientHub.Application.UseCases.Clients.Register;
 
 public class RegisterClientUseCase : IRegisterClientUseCase
 {
+    private readonly IClientWriteOnlyRepository _clientWriteOnlyRepository;
+    private readonly IClientReadOnlyRepository _clientReadOnlyRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RegisterClientUseCase(IClientWriteOnlyRepository clientWriteOnlyRepository,
+        IUnitOfWork unitOfWork,
+        IClientReadOnlyRepository clientReadOnlyRepository)
+    {
+        _clientWriteOnlyRepository = clientWriteOnlyRepository;
+        _unitOfWork = unitOfWork;
+        _clientReadOnlyRepository = clientReadOnlyRepository;
+    }
+
     public async Task<ResponseClientJson> Execute(RequestClientJson request)
+    {
+        Validate(request);
+
+        var userExist = await _clientReadOnlyRepository.EmailAlreadyExists(request.Email);
+
+        if (userExist.IsTrue())
+        {
+            throw new EmailAlreadyExistsException("Já existe uma conta cadastrada com o e-mail informado.");
+        }
+
+        var entity = request.Adapt<Client>();
+        await _clientWriteOnlyRepository.Add(entity);
+        await _unitOfWork.Commit();
+
+        return new ResponseClientJson
+        {
+            Id = entity.Id,
+            Name = request.Name
+        };
+    }
+
+    private void Validate(RequestClientJson request)
     {
         var validator = new RegisterClientValidator();
 
@@ -18,11 +57,5 @@ public class RegisterClientUseCase : IRegisterClientUseCase
             var errors = validationResult.Errors.Select(failure => failure.ErrorMessage).ToList();
             throw new ErrorOnValidationException(errors);
         }
-
-        return new ResponseClientJson
-        {
-            Id = Guid.NewGuid(),
-            Name = request.Name
-        };
     }
 }
