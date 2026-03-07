fdbc4192db38a58dde24ae022437915626ad9bab
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-07 10:07:49 -0300

    feat: utilizando Mapster para mapear os dados

---
+++ ProductClientHub.Application/Services/Mapping/MapConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+using ProductClientHub.Communication.Requests;
+
+namespace ProductClientHub.Application.Services.Mapping;
+
+public static class MapConfigurations
+{
+    public static void Configure()
+    {
+        TypeAdapterConfig<RequestClientJson, Domain.Entities.Client>
+            .NewConfig()
+            .Ignore(c => c.Id);
+    }
+}
