3b92ad7fe508e4e28b97e116cf347073aef870ca
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-07 10:09:10 -0300

    feat: verificação se o e-mail já existe no banco de dados

---
+++ ProductClientHub.Exceptions/ExceptionsBase/EmailAlreadyExistsException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace ProductClientHub.Exceptions.ExceptionsBase;
+
+public class EmailAlreadyExistsException : ProductClientHubException
+{
+    public EmailAlreadyExistsException(string errorMessage) : base(errorMessage)
+    {
+    }
+
+    public override List<string> GetErrors() => new List<string> { Message };
+
+    public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.BadRequest;
+}
