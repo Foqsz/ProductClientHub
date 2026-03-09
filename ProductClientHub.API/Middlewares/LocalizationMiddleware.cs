a6f1d66453db803cbf5cd80a272c375dfe263e8f
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-09 10:02:42 -0300

    feat: Localization Middleware

---
+++ ProductClientHub.API/Middlewares/LocalizationMiddleware.cs
@@ -0,0 +1,34 @@
+using ProductClientHub.Domain.Extensions;
+using System.Globalization;
+
+namespace ProductClientHub.API.Middlewares;
+
+public class LocalizationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public LocalizationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguage = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var cultureInfo = new CultureInfo("pt-BR");
+
+        if (requestedCulture.NotEmpty()
+            && supportedLanguage.Exists(c => c.Name.Equals(requestedCulture)))
+        {
+            cultureInfo = new CultureInfo(requestedCulture);
+        }
+
+        CultureInfo.CurrentCulture = cultureInfo;
+        CultureInfo.CurrentUICulture = cultureInfo;
+
+        await _next(context);
+    }
+}
