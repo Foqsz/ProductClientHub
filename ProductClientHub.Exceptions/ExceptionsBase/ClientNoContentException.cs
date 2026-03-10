ce884573741ec936416640c309aa5fbc6ffbfc04
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-10 09:30:40 -0300

    feat: Client No Content exception

---
+++ ProductClientHub.Exceptions/ExceptionsBase/ClientNoContentException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace ProductClientHub.Exceptions.ExceptionsBase;
+
+public class ClientNoContentException : ProductClientHubException
+{
+    public ClientNoContentException(string errorMessage) : base(errorMessage)
+    {
+    }
+
+    public override List<string> GetErrors() => new List<string> { Message };
+
+    public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.NoContent;
+}
