2d8d5d30dbbc5086c07b9c2e7af3d998f2c7c761
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-10 09:33:09 -0300

    feat: use Cases

---
+++ ProductClientHub.Application/UseCases/GetAll/GetAllClientsUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Mapster;
+using ProductClientHub.Communication.Responses;
+using ProductClientHub.Domain.Extensions;
+using ProductClientHub.Domain.Repositories.Client.Register;
+using ProductClientHub.Exceptions.ExceptionsBase;
+
+namespace ProductClientHub.Application.UseCases.GetAll;
+
+public class GetAllClientsUseCase : IGetAllClientsUseCase
+{
+    private readonly IClientReadOnlyRepository _clientReadOnlyRepository;
+
+    public GetAllClientsUseCase(IClientReadOnlyRepository clientReadOnlyRepository)
+    {
+        _clientReadOnlyRepository = clientReadOnlyRepository;
+    }
+
+    public async Task<ResponseAllClientsJson> Execute()
+    {
+        var clients = await _clientReadOnlyRepository.GetAll();
+
+        if (clients.Any().IsFalse())
+            throw new ClientNoContentException(ResourceMessagesExceptions.CLIENT_NOCONTENT);
+        
+        var mapping = clients.Adapt<List<ResponseClientJson>>();
+
+        return new ResponseAllClientsJson
+        {
+            Clients = mapping 
+        };
+    }
+}
---
+++ ProductClientHub.Application/UseCases/GetAll/IGetAllClientsUseCase.cs
@@ -0,0 +1,8 @@
+﻿using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.Application.UseCases.GetAll;
+
+public interface IGetAllClientsUseCase
+{
+    Task<ResponseAllClientsJson> Execute();
+}
