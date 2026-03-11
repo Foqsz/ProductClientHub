e7864b6a98170bb904e728a653755b90f14442a8
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-11 11:38:33 -0300

    feat: validação para saber se exist client com o id informado

---
+++ ProductClientHub.Application/UseCases/Products/Register/IRegisterProductUseCase.cs
@@ -0,0 +1,9 @@
+﻿using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.Application.UseCases.Products.Register;
+
+public interface IRegisterProductUseCase
+{
+    Task<ResponseProductJson> Execute(Guid clientId, RequestProductJson request);
+}
