4861219803fde26517661e8292bb039907e128aa
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-14 12:48:27 -0300

    feat: getAll Products use Cases

---
+++ ProductClientHub.Application/UseCases/Products/GetAll/GetAllProductsUseCase.cs
@@ -0,0 +1,29 @@
+﻿using Mapster;
+using ProductClientHub.Communication.Responses;
+using ProductClientHub.Domain.Repositories.Product;
+using ProductClientHub.Exceptions.ExceptionsBase;
+
+namespace ProductClientHub.Application.UseCases.Products.GetAll;
+
+public class GetAllProductsUseCase : IGetAllProductsUseCase
+{
+    private readonly IProductsReadOnlyRepository _productReadOnlyRepository;
+
+    public GetAllProductsUseCase(IProductsReadOnlyRepository productReadOnlyRepository)
+    {
+        _productReadOnlyRepository = productReadOnlyRepository;
+    }
+
+    public async Task<ResponseProductsJson> Execute()
+    {
+        var products = await _productReadOnlyRepository.GetAll();
+
+        if (products is null || !products.Any())
+            throw new NotFoundException(ResourceMessagesExceptions.PRODUCT_NOTFOUND);
+
+        return new ResponseProductsJson
+        {
+            Products = products.Adapt<List<ResponseShortProductJson>>()
+        };
+    }
+}
