40d6a3adbfd550344e7928ff75362b0c8db2951b
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-16 11:41:20 -0300

    feat: Open Api documentação

--- ProductClientHub.API/Program.cs
+++ ProductClientHub.API/Program.cs
@@ -8,6 +8,40 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddOpenApi("v1", o =>
+//{
+//    o.AddDocumentTransformer((document, context, CancellationToken) =>
+//    {
+//        document.Info = new()
+//        {
+//            Title = "Product Client Hub API",
+//            Version = "v1",
+//            Description = "API for managing product clients and their interactions.",
+//            Contact = new()
+//            {
+//                Name = "Product Client Hub Team",
+//                Email = "teste@gmail.com"
+//            }
+//        };
+
+//        document.Servers =
+//        [
+//            new()
+//            {
+//                Url = "https://localhost:5001",
+//                Description = "Local Development Server"
+//            },
+//        ];
+
+//        document.ExternalDocs = new()
+//        {
+//            Description = "API Documentation",
+//            Url = new Uri("https://localhost:5001/swagger/index.html")
+//        };
+
+//        return Task.CompletedTask;
+//    });
+//});
 
 //Add global exception filter
 builder.Services.AddMvc(option => option.Filters.Add(typeof(ExceptionFilter)));
@@ -28,6 +62,8 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<LocalizationMiddleware>();
 
 app.UseHttpsRedirection();
+
+app.MapOpenApi("/doc/{documentName}.json");
 
 app.UseAuthorization();
 
