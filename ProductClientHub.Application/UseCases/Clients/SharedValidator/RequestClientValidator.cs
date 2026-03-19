78e6955ef46c90c7b68120633747e060d135b395
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-19 09:53:52 -0300

    feat: validator do register atualizado

--- ProductClientHub.Application/UseCases/Clients/SharedValidator/RequestClientValidator.cs
+++ ProductClientHub.Application/UseCases/Clients/SharedValidator/RequestClientValidator.cs
@@ -18,5 +18,13 @@ public class RequestClientValidator : AbstractValidator<RequestClientJson>
             .WithMessage("O e-mail é obrigatório e não pode ser vazio.")
             .EmailAddress()
             .WithMessage("O e-mail deve ser um endereço de email válido.");
+
+        RuleFor(client => client.Password)
+            .NotEmpty()
+            .WithMessage("A senha é obrigatória e não pode ser vazia.")
+            .MinimumLength(6)
+            .WithMessage("A senha deve conter no mínimo 6 caracteres.")
+            .MaximumLength(100)
+            .WithMessage("A senha deve conter no máximo 100 caracteres.");
     }
 }
