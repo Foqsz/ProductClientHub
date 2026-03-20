f7eaa70583eee263f329cfa1387b5cd32697e315
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-20 10:33:58 -0300

    feat: new request

---
+++ ProductClientHub.Comunication/Requests/RequestChangePassword.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductClientHub.Communication.Requests;
+
+public class RequestChangePassword
+{
+    public string CurrentPassword { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+}
