0787ff1743559752300347af812263cb96540c58
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-24 12:22:08 -0300

    feat: controller login

---
+++ ProductClientHub.API/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.Application.UseCases.DoLogin;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseTokenJson), StatusCodes.Status200OK)]
+    public async Task<IActionResult> DoLogin([FromBody] RequestLoginJson request, [FromServices] IDoLoginUseCase useCase)
+    {
+        var response = await useCase.Execute(request);
+
+        return Ok(response);
+    }
+}
