f1dc4fc2ce3b8b339d2314702dad58a5694e9be6
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-24 12:22:40 -0300

    feat: jwtTokenValidator

---
+++ ProductClientHub.Domain/Security/Tokens/IAccessTokenValidator.cs
@@ -0,0 +1,6 @@
+﻿namespace ProductClientHub.Domain.Security.Tokens;
+
+public interface IAccessTokenValidator
+{
+    public Guid ValidateAnGetUserIdentifier(string token);
+}
---
+++ ProductClientHub.Infrastructure/Security/Tokens/Acess/Validator/JwtTokenValidator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using ProductClientHub.Domain.Security.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ProductClientHub.Infrastructure.Security.Tokens.Acess.Validator;
+
+public class JwtTokenValidator : JwtTokenHandler, IAccessTokenValidator
+{
+    private readonly string _signingKey;
+
+    public JwtTokenValidator(string signingKey) => _signingKey = signingKey;
+
+    public Guid ValidateAnGetUserIdentifier(string token)
+    {
+        var validationParameter = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            IssuerSigningKey = SecurityKey(_signingKey),
+            ClockSkew = new TimeSpan(0)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var principal = tokenHandler.ValidateToken(token, validationParameter, out _);
+
+        var userIdentifier = principal.Claims.First(c => c.Type == ClaimTypes.Sid).Value;
+
+        return Guid.Parse(userIdentifier);
+    }
+}
