5a284b732939bc510f26bf650f8c18f39e5d4219
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-25 11:26:44 -0300

    feat: aqui eu pego o token via TokenOnRequest e depois faço as validaçõs no OnAuthorizationAsync

---
+++ ProductClientHub.API/Filters/AuthenticatedUserFilter.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using ProductClientHub.Communication.Responses;
+using ProductClientHub.Domain.Extensions;
+using ProductClientHub.Domain.Repositories.Client;
+using ProductClientHub.Domain.Security.Tokens;
+using ProductClientHub.Exceptions.ExceptionsBase;
+
+namespace ProductClientHub.API.Filters;
+
+public class AuthenticatedUserFilter : IAsyncAuthorizationFilter
+{
+    private readonly IAccessTokenValidator _accessTokenValidator;
+    private readonly IClientReadOnlyRepository _repository;
+
+    public AuthenticatedUserFilter(IAccessTokenValidator accessTokenValidator, IClientReadOnlyRepository repository)
+    {
+        _accessTokenValidator = accessTokenValidator;
+        _repository = repository;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        try
+        {
+            var token = TokenOnRequest(context);
+
+            var userIdentifier = _accessTokenValidator.ValidateAnGetUserIdentifier(token);
+
+            var exist = await _repository.ExistActiveClientWithIdentifier(userIdentifier);
+            if (exist.IsFalse())
+            {
+                throw new ExistActiveUserWithIdentifierException(ResourceMessagesExceptions.USER_WITHOUT_PERMISSION_ACCESS_RESOURCE);
+            }
+        }
+        catch (SecurityTokenExpiredException)
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson("TokenIsExpired")
+            {
+                TokenIsExpired = true,
+            });
+        }
+        catch (ProductClientHubException ex)
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson(ex.Message));
+        }
+        catch
+        {
+            context.Result = new UnauthorizedObjectResult(new ResponseErrorJson(ResourceMessagesExceptions.USER_WITHOUT_PERMISSION_ACCESS_RESOURCE));
+        }
+    }
+
+    private static string TokenOnRequest(AuthorizationFilterContext context)
+    {
+        var authentication = context.HttpContext.Request.Headers.Authorization.ToString();
+        if (string.IsNullOrWhiteSpace(authentication))
+        {
+            throw new TokenIsNullOrEmptyException(ResourceMessagesExceptions.NO_TOKEN);
+        }
+
+        //retornar o token sem a palavra extra "bearer ".
+        return authentication["Bearer ".Length..].Trim();
+    }
+}
