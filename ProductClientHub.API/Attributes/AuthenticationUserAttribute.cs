0ecdaf615a088c78043e6d24814e1334d868b248
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-25 11:25:11 -0300

    feat: atributte para usar nos controles

---
+++ ProductClientHub.API/Attributes/AuthenticationUserAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.API.Filters;
+
+namespace ProductClientHub.API.Attributes;
+
+public class AuthenticationUserAttribute : TypeFilterAttribute
+{
+    public AuthenticationUserAttribute() : base(typeof(AuthenticatedUserFilter))
+    {
+    }
+}
