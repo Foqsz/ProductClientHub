3a3d518b68e0be3541c44ee728249334a365be59
Foqsz <contatovictorvinicius05@gmail.com>
2026-03-26 11:16:31 -0300

    feat: iniciando a validação de usuário logado

--- ProductClientHub.Application/UseCases/Products/GetAll/GetAllProductsUseCase.cs
+++ ProductClientHub.Application/UseCases/Products/GetAll/GetAllProductsUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using Mapster;
 using ProductClientHub.Communication.Responses;
 using ProductClientHub.Domain.Repositories.Product;
+using ProductClientHub.Domain.Services.LoggedUser;
 using ProductClientHub.Exceptions.ExceptionsBase;
 
 namespace ProductClientHub.Application.UseCases.Products.GetAll;
@@ -8,14 +9,21 @@ namespace ProductClientHub.Application.UseCases.Products.GetAll;
 public class GetAllProductsUseCase : IGetAllProductsUseCase
 {
     private readonly IProductsReadOnlyRepository _productReadOnlyRepository;
+    private readonly ILoggedUser _loggedUser;
 
-    public GetAllProductsUseCase(IProductsReadOnlyRepository productReadOnlyRepository)
+    public GetAllProductsUseCase(IProductsReadOnlyRepository productReadOnlyRepository, ILoggedUser loggedUser)
     {
         _productReadOnlyRepository = productReadOnlyRepository;
+        _loggedUser = loggedUser;
     }
 
     public async Task<ResponseProductsJson> Execute()
     {
+        var user = await _loggedUser.User();
+
+        if(user is null)
+            throw new UserNotLoggedException(ResourceMessagesExceptions.NOT_LOGGED);
+
         var products = await _productReadOnlyRepository.GetAll();
 
         if (products is null || !products.Any())
