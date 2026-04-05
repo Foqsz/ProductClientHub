f135fb32b83c9d7438e89b211a585cff13b2d658
Foqsz <contatovictorvinicius05@gmail.com>
2026-04-04 23:25:05 -0300

    #
    Add test for client not found in DeleteClientUseCase
    
    Added DeleteClientUseCase_NotFound unit test to verify that deleting a non-existent client throws NotFoundException with the correct message. Also updated using directives and improved formatting in CreateUseCase for consistency.

--- UseCase.Test/Client/Delete/DeleteClientUseCaseTest.cs
+++ UseCase.Test/Client/Delete/DeleteClientUseCaseTest.cs
@@ -1,6 +1,7 @@
 ﻿using CommonTestUtilities.Entities;
 using CommonTestUtilities.Repositories;
 using ProductClientHub.Application.UseCases.Users.Delete;
+using ProductClientHub.Exceptions.ExceptionsBase;
 using Shouldly;
 
 namespace UseCase.Test.Client.Delete;
@@ -19,13 +20,30 @@ public class DeleteClientUseCaseTest
         await result.ShouldNotBeNull();
     }
 
+    [Fact]
+    public async Task DeleteClientUseCase_NotFound()
+    {
+        var (client, _) = ClientBuilder.Build();
+
+        var useCase = CreateUseCase(client);
+
+        client.Id = Guid.NewGuid();
+
+        var result = await Should.ThrowAsync<NotFoundException>(async () =>
+        {
+            await useCase.Execute(client.Id);
+        });
+
+        result.Message.ShouldBe(ResourceMessagesExceptions.CLIENT_NOCONTENT);
+    }
+
     private static DeleteClientUseCase CreateUseCase(ProductClientHub.Domain.Entities.Client? client)
     {
         var deleteWriteOnlyRepository = ClientDeleteAccountOnlyRepositoryBuilder.Build();
         var repositoryReadOnly = new ClientReadOnlyRepositoryBuilder();
         var unitOfWork = UnitOfWorkBuilder.Build();
 
-        if(client is not null)
+        if (client is not null)
         {
             repositoryReadOnly.GetById(client);
             deleteWriteOnlyRepository.Delete(client.Id);
